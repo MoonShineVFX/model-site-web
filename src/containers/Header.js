8bfae1fd796f59a9ecfbeccc4df0ec55ccd9070c
bliang.tw <bliang.tw@moonshine.ad>
2021-11-22 18:34:08 +0800

    - Add cart icon in header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import { styled } from '@mui/system';
 import { Toolbar, Box } from '@mui/material';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { ButtonLink } from '../components/Links';
 import Logo from '../components/Logo';
+import FontIcon from '../components/FontIcon';
 import Navbar from './Navbar';
 import deftag from '../utils/util.deftag';
 
@@ -25,26 +28,54 @@ const HeaderLayout = styled(Toolbar)(({ theme }) => ({
     },
 }));
 
+// 購物車 icon
+const ShoppingCartLayout = styled('div')(({ theme }) => ({
+    fontSize: '1.4em',
+    marginRight: '30px',
+    cursor: 'pointer',
+    '.count': {
+        marginLeft: '10px',
+    },
+}));
+
 //
-const Header = () => (
+const Header = () => {
 
-    <AppBarLayout>
-        <HeaderLayout className="Model-container">
-            <Logo />
+    // useEffect(() => {
 
-            <Box sx={{ flexGrow: 1 }}>
-                <Navbar />
-            </Box>
+    //     localStorage.getItem('cartItem');
 
-            <Box sx={{ display: { xs: 'flex', md: 'flex' } }}>
-                <ButtonLink
-                    url="signin"
-                    text={text_signin}
-                />
-            </Box>
-        </HeaderLayout>
-    </AppBarLayout>
+    // }, []);
 
-);
+    return (
+
+        <AppBarLayout>
+            <HeaderLayout className="Model-container">
+                <Logo />
+
+                <Box sx={{ flexGrow: 1 }}>
+                    <Navbar />
+                </Box>
+
+                <Box sx={{
+                    display: { xs: 'flex', md: 'flex' },
+                    alignItems: 'center',
+                }}>
+                    <ShoppingCartLayout>
+                        <FontIcon icon={faShoppingCart} />
+                        <span className="count">({0})</span>
+                    </ShoppingCartLayout>
+
+                    <ButtonLink
+                        url="signin"
+                        text={text_signin}
+                    />
+                </Box>
+            </HeaderLayout>
+        </AppBarLayout>
+
+    );
+
+};
 
 export default Header;

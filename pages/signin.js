edb8fc9b218cc4dea02d5a7a4d9ea91c1ba56f80
bliang.tw <homebetty0125@gmail.com>
2022-07-07 16:33:01 +0800

    - Fixed signin issue

--- pages/signin.js
+++ pages/signin.js
@@ -4,7 +4,6 @@ import {
     useEffect,
     useState,
 } from 'react';
-import dynamic from 'next/dynamic';
 import { useForm } from 'react-hook-form';
 
 import {
@@ -23,16 +22,10 @@ import { GlobalContext } from '../src/context/global.state';
 import util from '../src/utils/util';
 import utilConst from '../src/utils/util.const';
 import Service from '../src/utils/util.service';
-// import useReCaptchaVerify from '../src/utils/useReCaptchaVerify';
+import useReCaptchaVerify from '../src/utils/useReCaptchaVerify';
 
 const { redirectTo } = util;
 const { paswdConfig } = utilConst;
-
-// dynamic
-const useReCaptchaVerify = dynamic(() => import('../src/utils/useReCaptchaVerify'), {
-    loading: () => <Loading />,
-    ssr: false,
-});
 
 const Signin = ({ langs }) => {
 
